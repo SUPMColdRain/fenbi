a926a4e53319baac1547180172a204f565c34627
SUPMColdRain <friedenwang@outlook.com>
2022-02-08 11:57:00 +0800

    auto update

---
+++ export_deck.py
@@ -0,0 +1,38 @@
+import time
+import git
+from pywinauto.application import Application
+from pywinauto.keyboard import send_keys
+
+
+def export_deck():
+    anki_path = "C:\\Program Files\\Anki\\anki.exe"
+    # subprocess.Popen(anki_path)
+    Application().start(anki_path)
+    app = Application(backend="uia").connect(path=anki_path, title='账户1 - Anki')
+    anki = app.window(title='账户1 - Anki')
+    time.sleep(2)
+    anki.type_keys("^e")
+    anki.child_window(title='导出').type_keys("{DOWN 2}")
+    anki.child_window(title='导出').type_keys("{TAB}")
+    anki.child_window(title='导出').type_keys("{DOWN 11}")
+    anki.child_window(title='导出').type_keys("{TAB}")
+    anki.child_window(title='导出').type_keys("{SPACE}")
+    anki.child_window(title='导出').type_keys("{TAB}")
+    anki.child_window(title='导出').type_keys("{SPACE}")
+    anki.child_window(title='导出').type_keys("{TAB}")
+    anki.child_window(title='导出').type_keys("{ENTER}")
+    send_keys("{ENTER 2}")
+    anki.close()
+
+
+def auto_git_push():
+    g = git.Repo('').git
+    g.add("--all")
+    g.commit("-m auto update")
+    g.push()
+    print("Successful push!")
+
+
+if __name__ == '__main__':
+    # export_deck()
+    auto_git_push()
