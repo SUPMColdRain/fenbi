c87f2df123ec1a5328ddb680e484c1f361988296
SUPMColdRain <friedenwang@outlook.com>
2022-02-08 16:39:07 +0800

    auto update star question

--- git_push.sh
+++ git_push.sh
@@ -1,5 +1,9 @@
+python main.py
+# git config -e
+# change HTTPS: `https://github.com/<name>/<repo>.git`
+# to SSH: `git@github.com:<name>/<repo>.git`
 echo "--- git push start! ---"
 git add .
-git commit -m "auto update"
+git commit -m "auto update star question"
 git push origin master
 echo "---- git push end! ----"
